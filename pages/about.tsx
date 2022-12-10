a9dc63ef198a5c906d5aa3d9f29caec11f16e795
Luigi Barbato <luigi.barbato@sighup.io>
2022-12-10 23:28:41 +0100

    chore: initial responsive refactor

--- pages/about.tsx
+++ pages/about.tsx
@@ -6,7 +6,7 @@ const About: NextPage = () => {
     return (
         <div className="flex flex-col h-screen">
             <Header />
-            <article className="prose lg:prose-xl m-auto">
+            <article className="prose lg:prose-xl p-10 md:p-0 md:m-auto">
                 <h1>Garlic bread with cheese: What the science tells us</h1>
                 <p>
                     Pensiamo che il mondo debba essere un posto migliore e quindi libero. Questo progetto nasce sopratutto da un bisogno personale: cercare contenuti in maniera completamente libera e autonoma. Nessuna targetizzazione, nessun ads, nessun reccomender system, nessun algoritmo che sceglie per noi.
