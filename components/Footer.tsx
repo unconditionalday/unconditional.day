34ebb13f7261c0ad23f0bed6293d8c848d2bdb32
Luigi Barbato <luigi.barbato@sighup.io>
2022-12-11 17:49:20 +0100

    chore: improve layout

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
     return (
-        <footer className="footer flex flex-col md:flex-row justify-between md:p-10 text-black border">
+        <footer className="footer flex flex-col md:flex-row justify-between p-2 md:p-10 text-black border">
             <div className="subfooter-left flex flex-col md:flex-col md:gap-2 px-2 md:px-10">
                 <h1 className="title text-xl md:text-2xl font-black rounded-lg">Unconditional.</h1>
                 <p className="text-transparent text-md md:text-xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 ">© 2022 All rights reserved.</p>
