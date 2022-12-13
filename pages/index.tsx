9206ba35b68ac7a850125a7595b5508d3a41f252
Luigi Barbato <luigi.barbato@sighup.io>
2022-12-13 23:38:32 +0100

    feat: add favicons and webmanifest

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,7 +32,24 @@ const Home: NextPage = () => {
       <Head>
         <title>Unconditional</title>
         <meta name="description" content="Unconditional" />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <div className="flex flex-col min-h-screen">
@@ -70,9 +87,9 @@ const Home: NextPage = () => {
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
                 ></path>
               </svg>
