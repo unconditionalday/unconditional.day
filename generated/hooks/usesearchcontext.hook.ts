ce56a02b216892dde4db97a658826e75e8fac52c
Luigi Barbato <work@luigibarbato.digital>
2023-10-14 14:43:28 +0200

    feat: integrate swr hook and enhance style

---
+++ generated/hooks/usesearchcontext.hook.ts
@@ -0,0 +1,51 @@
+import useSWR, { Fetcher } from "swr";
+import { SearchContextDetails } from "../models/SearchContextDetails";
+import { DefaultService } from "../services/DefaultService";
+import { KeyedMutator } from "swr/_internal";
+import { FeedItem } from "../models/FeedItem";
+
+type FetcherInput = {
+  key: string;
+  query: string | undefined;
+};
+
+const fetcher: Fetcher<SearchContextDetails, FetcherInput> = ({ key, query }) =>
+  DefaultService.getV1SearchContext(query);
+
+type IUseSearchContextDetailsOutcome = {
+  searchContextDetails: SearchContextDetails | undefined;
+  error: any;
+  isLoading: boolean;
+  mutate: KeyedMutator<SearchContextDetails>;
+  isEmpty: boolean;
+};
+type IUseSearchContextDetails = {
+  (query: string | undefined): IUseSearchContextDetailsOutcome;
+};
+
+const useSearchContext: IUseSearchContextDetails = (
+  query: string | undefined,
+) => {
+  const key = query ? `context_${query}` : null;
+  const fetcherInput = { key, query };
+  const { data, error, isLoading, mutate } = useSWR(key, () =>
+    fetcher(fetcherInput),
+  );
+
+  const validData =
+    data &&
+    data.language != "" &&
+    data.link != "" &&
+    data.summary != "" &&
+    data.thumbnail;
+
+  return {
+    searchContextDetails: data,
+    error,
+    isLoading,
+    mutate,
+    isEmpty: !isLoading && !validData,
+  };
+};
+
+export default useSearchContext;
